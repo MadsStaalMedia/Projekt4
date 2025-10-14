c39cdd9c06ba22ae1de6350ae5fa77d0e728ff32
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-10-14 09:57:09 +0200

    start på aktivitetsliste

--- js/aktivitetsliste.js
+++ js/aktivitetsliste.js
@@ -1,2 +1,9 @@
-const aktiviteter = ["netOd", "streg", "lego", "klub", "natur", "netSve", "cafe", "50+"];
+const events = ["netOd", "streg", "lego", "klub", "natur", "netSve", "cafe", "50+"];
 
+let eventList = document.getElementById("eventList");
+
+for (let i = 0; i < events.length ; i++) {
+    console.log(i)
+    let x = events[i];
+    eventList.innerHTML += '<button class="eventButton"></button>'
+}
