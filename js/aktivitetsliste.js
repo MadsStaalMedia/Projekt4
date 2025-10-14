bc72e60df07370ddda8817783f1b423fd347d32a
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-10-14 10:38:18 +0200

    Fik opsat første billede til knapperne

--- js/aktivitetsliste.js
+++ js/aktivitetsliste.js
@@ -1,6 +1,14 @@
-const netOd = "<p><b>Netværksgruppe</b> - Odense</p>"
+const netOd = '<img class="eventButton__img" src="billeder/ikoner/netvaerk1.svg" alt="netærk ikon"><p><b>Netværksgruppen</b> - Odense</p>';
+const streg = '<p><b>ADHD Streger</b> - Odense</p>';
+const lego = '<p><b>LEGO Cafe</b> - Bolbro</p>';
+const klub = '<p><b>ADHD Klubben</b> - Middelfart</p>';
+const natur = '<p><b>Naturgruppen</b> - Svendborg</p>';
+const netSve = '<p><b>Netværksgruppen</b> - Svendborg</p>';
+const cafe = '<p><b>Cafe Ærø</b> - Ærø</p>';
+const halvtre = '<p><b>50+ Netværksgruppen</b><br> (mangler tovholder) </p>';
 
-const events = [netOd, "streg", "lego", "klub", "natur", "netSve", "cafe", "50+"];
+
+const events = [netOd, streg, lego, klub, natur, netSve, cafe, halvtre];
 
 let eventList = document.getElementById("eventList");
 
@@ -13,3 +21,4 @@ for (let i = 0; i < events.length ; i++) {
 function seeEvent(x) {
 
 }
+
