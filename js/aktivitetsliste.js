82b73b65bc880731e02fa09c4ff9459b15b54747
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-10-14 14:43:58 +0200

    Begyndt arbejde på tidsregning

--- js/aktivitetsliste.js
+++ js/aktivitetsliste.js
@@ -7,7 +7,6 @@ const netSve = '<img class="eventButton__img" src="billeder/ikoner/netvaerk2.svg
 const cafe = '<img class="eventButton__img" src="billeder/ikoner/cafe.svg" alt="cafe ærø ikon"><p><b>Cafe Ærø</b> - Ærø</p>';
 const halvtre = '<img class="eventButton__img" src="billeder/ikoner/tovholder.svg" alt="50+ netværksgruppen ikon"><p><b>50+ Netværksgruppen</b><br> (mangler tovholder) </p>';
 
-
 const events = [netOd, streg, lego, klub, natur, netSve, cafe, halvtre];
 
 let eventList = document.getElementById("eventList");
@@ -15,31 +14,29 @@ let eventList = document.getElementById("eventList");
 let eventDetail = document.getElementById("eventDetail");
 
 for (let i = 0; i < events.length ; i++) {
-    console.log(i);
     let x = events[i];
     eventList.innerHTML += '<button class="eventButton" onclick="seeEvent('+i+')">' + x + '</button>';
-    eventDetailing(i);
+    
 }
 
-function eventDetailing(x) {
-    if (x == 0) {
-        eventDetail.innerHTML +=
-        eventDetail.innerHTML += '<h3>Netværksgruppe for kvinder</h3><p>Mødes sidste onsdag i måneden, man er velkommen både som diagnosticeret eller afventende udredning. Der er altid nye og vi har en god og rummende stemning.<br><br>De næste netværksmøder er:</p><ul><li>Onsdag d. 29/10</li><li>Onsdag d. 26/11</li></ul>';
-    } else if (x == 1) {
-        
-    } else if (x == 2) {
-        
-    } else if (x == 3) {
-        
-    } else if (x == 4) {
-        
-    } else if (x == 5) {
-        
-    } else if (x == 6) {
+ function eventDate() {
+    let date = new Date();
+    let firstDay = new Date(date.getFullYear(),date.getMonth(),1);
+    console.log(firstDay);
 
-    } else if (x == 7) {
+    for (i = firstDay.getDate(); i < 8; i++) {
+        console.log(i);
 
+        if (firstDay.getDay() == 2) {
+        const nextTuesday = firstDay.getDate() + firstDay.getMonth();
+        document.getElementById('event1date1').innerText += nextTuesday;
+        console.log(nextTuesday);
+        }
+
+        firstDay.setDate(firstDay.getDate() + 1);
+        console.log(firstDay);
     }
+
     
 }
 
