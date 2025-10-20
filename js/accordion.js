7fa72b2d5d56a80d53b160bf4fe235f07f9ab31b
svendsteen <svendkj2@hotmail.com>
2025-10-20 09:57:53 +0200

    update

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,25 @@
-const acc = [
-    '<button class="accordion__section">Hvad kan jeg lave?</button>',
-    '<button class="accordion__section">Hvor meget skal jeg arbejde?</button>',
-    '<button class="accordion__section">Hvad hvis jeg bor langt væk?</button>',
-    '<button class="accordion__section">Hvordan bliver jeg frivillig?</button>',
-    '<button class="accordion__section">Endnu et intelligent spørgsmål</button>'
-];
+const acc = document.getElementsByClassName("accordion__section");
 
+for (let i=0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() { alert(lol)
+        this.classlist.toggle("active");
 
-for (let i = 0; i < acc.length; i++) {
-    
-    document.getElementById("accordion").innerHTML += acc[i];
-    
+        let pan = this.nextElementSibling;
+
+        if (pan.style.display === "block" ) {
+            pan.style.display = "none";
+        } else {
+            pan.style.display = "block";
+        }
+    });
+}
+
+function myFunction () {
+    let pan = this.nextElementSibling;
+
+        if (pan.style.display === "none" ) {
+            pan.style.display = "block";
+        } else {
+            pan.style.display = "none";
+        }
 }
