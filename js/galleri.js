5dcc5b1c62bdacb663d6755e80ba58866dcb73b5
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-10-22 19:50:47 +0200

    optimering af billeder

--- js/galleri.js
+++ js/galleri.js
@@ -1,4 +1,4 @@
-const billeder = ["billeder/carousel/carousel1.webp","billeder/carousel/carousel2.png","billeder/carousel/carousel3.png"];
+const billeder = ["billeder/carousel/carousel1.webp","billeder/carousel/carousel2.webp","billeder/carousel/carousel3.webp"];
 
 let prikviser = document.getElementById("carousel_prikker");
 let billedeviser = document.getElementById("carousel_billede");
