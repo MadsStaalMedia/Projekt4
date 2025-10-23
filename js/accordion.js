747bab43fa53947e45c0c5c805a012edc3ffd71d
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-10-23 08:37:25 +0200

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -4,7 +4,6 @@ const acc = [
     document.getElementById("section1"),
     document.getElementById("section2"),
     document.getElementById("section3"),
-    document.getElementById("section4"),
 ]
 
 for (i=0; i < acc.length; i++) {
